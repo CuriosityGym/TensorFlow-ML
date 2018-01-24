d0f288be34b55147754e05c90a380006fd6568c1
rupin <rupin.chheda@gmail.com>
2018-01-24 11:53:44 +0530

    Deploy

--- keras_first_network.py
+++ keras_first_network.py
@@ -1,10 +1,9 @@
-# Create first network with Keras
+# Create your first MLP in Keras
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 # fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
+numpy.random.seed(7)
 # load pima indians dataset
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
@@ -12,15 +11,13 @@ X = dataset[:,0:8]
 Y = dataset[:,8]
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
 model.fit(X, Y, epochs=150, batch_size=10)
-# calculate predictions
-predictions = model.predict(X)
-# round predictions
-rounded = [round(x[0]) for x in predictions]
-print(rounded)
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
