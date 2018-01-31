ec6c889a94bed5f9774af5bee6f5689560d5b8cf
rupin <rupin.chheda@gmail.com>
2018-01-31 13:04:44 +0530

    Curtailed CSV

--- keras_first_network.py
+++ keras_first_network.py
@@ -1,6 +1,6 @@
 # Create your first MLP in Keras
-#from keras.models import Sequential
-#from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dense
 import numpy
 # fix random seed for reproducibility
 numpy.random.seed(7)
