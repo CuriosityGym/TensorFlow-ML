9c8e802dc9f17f1e63e85b63b42aac95e5a5f275
rupin <rupin.chheda@gmail.com>
2018-01-31 12:54:28 +0530

    Curtailed CSV

--- keras_first_network.py
+++ keras_first_network.py
@@ -1,6 +1,6 @@
 # Create your first MLP in Keras
-from keras.models import Sequential
-from keras.layers import Dense
+#from keras.models import Sequential
+#from keras.layers import Dense
 import numpy
 # fix random seed for reproducibility
 numpy.random.seed(7)
@@ -10,14 +10,15 @@ dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 X = dataset[:,0:8]
 Y = dataset[:,8]
 # create model
-model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-# Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# Fit the model
-model.fit(X, Y, epochs=150, batch_size=10)
-# evaluate the model
-scores = model.evaluate(X, Y)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+##model = Sequential()
+##model.add(Dense(12, input_dim=8, activation='relu'))
+##model.add(Dense(8, activation='relu'))
+##model.add(Dense(1, activation='sigmoid'))
+### Compile model
+##model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+### Fit the model
+##model.fit(X, Y, epochs=150, batch_size=10)
+### evaluate the model
+##scores = model.evaluate(X, Y)
+##print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
